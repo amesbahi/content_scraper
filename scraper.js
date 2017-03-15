407a5110d8ddf6bba7c02519a42979a1e3a904f6
Alborz Mesbahi <alborzmesbahi@WTA-MBP163.local>
2017-03-15 13:06:20 -0400

    Added Cheerio and json2csv modules

--- scraper.js
+++ scraper.js
@@ -2,6 +2,8 @@
 
 // Include modules
 const fs = require('fs');
+const cheerio = require('cheerio');
+const json2csv = require('json2csv');
 
 // Program your scraper to check for a folder called ‘data’. If the folder doesn’t exist,
 // the scraper should create one. If the folder does exist, the scraper should do nothing.
