b933579b92dc797d57116a23a9d88fcb305ed6f0
Alborz Mesbahi <alborzmesbahi@WTA-MBP163.local>
2017-03-16 20:10:53 -0400

    Made get request

--- scraper.js
+++ scraper.js
@@ -1,13 +1,26 @@
-// 'use strict';
+'use strict';
 
 // Include modules
 const fs = require('fs');
 const cheerio = require('cheerio');
 const json2csv = require('json2csv');
+const http = require('http');
 
-// Program your scraper to check for a folder called ‘data’. If the folder doesn’t exist,
+// Program scraper to check for a folder called ‘data’. If the folder doesn’t exist,
 // the scraper should create one. If the folder does exist, the scraper should do nothing.
-
 if (!fs.existsSync('./data')) {
     fs.mkdirSync('./data');
 }
+
+// Connect to the API URL (http://shirts4mike.com/shirts.php)
+const request = http.get("http://www.shirts4mike.com/shirts.php", response => {
+    let body = "";
+    // Read the data
+    response.on('data', data => {
+        body += data.toString();
+    });
+
+    response.on('end', () => {
+
+    });
+});
